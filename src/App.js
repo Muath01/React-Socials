d5ca91fa422140ab6f5a5134571b7d5fe6d7f6e5
Muath <muath.khalifa@yahoo.com>
2022-10-19 21:08:46 +0100

    files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
+import { Button, styled, Typography } from '@mui/material';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
-
+import { Add, Settings } from '@mui/icons-material';
+import Sidebar from './components/Sidebar';
+import Feed from "./components/Feed"
+import Navbar from './components/Navbar';
+import Rightbar from './components/Rightbar';
 function App() {
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <Navbar />
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,38 +1,18 @@
+import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ThemeProvider, createTheme } from '@mui/material'
-import {orange, red} from "@mui/material/colors"
-import { color } from '@mui/system';
 
-
+import {theme} from "./theme"
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const theme = createTheme({
-  palette:{
-    primary:{
-      main:"#ccc"
-    },
-    myCustomColor:{
-      main: orange[400],
-      superDark: red[800],
-      superLight: orange[300]
-    }
-  }
-});
 
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
